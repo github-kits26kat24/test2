69e7d32fb2cfb760d481255e78949f2521c4d011
kits26kat24 <k.tijani@hotmail.com>
2023-05-23 20:49:12 +0100

    commit pem update

--- Route.Table.tf
+++ Route.Table.tf
@@ -3,10 +3,10 @@
 resource "aws_route_table" "testcase_example" {
   vpc_id = aws_vpc.testcase_main.id
 
- # route {
- #   cidr_block = "10.0.1.0/24"
- #   gateway_id = aws_internet_gateway.igw_example.id
- # }
+  # route {
+  #   cidr_block = "10.0.1.0/24"
+  #   gateway_id = aws_internet_gateway.igw_example.id
+  # }
 
   tags = {
     Name = "Internet-Gateway-Example"
